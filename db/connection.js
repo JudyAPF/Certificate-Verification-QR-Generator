9843196bf981a9034d63e679e31e7da9e746182e
JudyAPF <judyapf888@gmail.com>
2025-02-24 15:26:56 +0800

    3rd commit

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 const con = mysql.createConnection({
   host: "localhost",
-  database: "certificate_verification_code_generator",
+  database: "certificate_verification_qr_generator",
   password: "",
   user: "root",
 });
